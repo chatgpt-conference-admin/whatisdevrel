b4fe977f89371b0b5063582954bed1bf92111d22
chatgpt-conference-admin <123183546+chatgpt-conference-admin@users.noreply.github.com>
2023-01-21 13:49:27 +0000

    commit

--- components/MetaHead.js
+++ components/MetaHead.js
@@ -6,9 +6,9 @@ export default function MetaHead() {
       <title>ChatGPT Conf 2023</title>
       <meta
         name="description"
-        content="All you need to know about DevRel and how to get into it."
+        content="ChatGPT Conf 2023"
       />
-      <meta property="og:url" content={`https://www.whatisdevrel.com`} />
+      <meta property="og:url" content={`https://chatgpt-conf.vercel.app/`} />
       <meta property="og:site_name" content="WhatisDevRel.com" />
       <meta
         property="og:description"
@@ -17,18 +17,18 @@ export default function MetaHead() {
       <meta property="og:title" content="ChatGPT Conf 2023" />
       <meta
         property="og:image"
-        content="https://www.whatisdevrel.com/ogimage_new.png"
+        content="/chatgpt.png"
       />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@nutlope" />
-      <meta name="twitter:title" content="What is DevRel?" />
+      <meta name="twitter:site" content="@Chatgptconf" />
+      <meta name="twitter:title" content="ChatGPT Conf 2023" />
       <meta
         name="twitter:description"
         content="ChatGPT Conf 2023"
       />
       <meta
         name="twitter:image"
-        content="https://www.whatisdevrel.com/ogimage_new.png"
+        content="/chatgpt.png"
       />
     </Head>
   );
